ac4f60473421a964d40a2a88d908c3f444b42ee3
M4theusCM <matheus.menezes@sptech.school>
2026-02-24 20:19:38 -0300

    sessão de sobre nos completa

---
+++ public/js/nav.js
@@ -0,0 +1,11 @@
+var menu = document.getElementById("menu")
+var menuLateral = document.getElementById("menuLateral")
+
+
+function abrirMenu(){
+    menuLateral.style.display = "flex"
+}
+
+function fecharMenu(){
+    menuLateral.style.display = "none"
+}
