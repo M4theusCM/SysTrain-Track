cb3420e9a97eab1922f303a33c87699b7ae2bba5
ribeirokk <luiz.ribeiro@sptech.school>
2026-02-26 14:36:28 -0300

    Telas de login e cadastro concluídas

---
+++ src/controllers/empresaController.js
@@ -0,0 +1,98 @@
+var empresaModel = require("../models/empresaModel");
+
+function cadastrarEmpresa(req, res) {
+    // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
+    var razaoSocial = req.body.razaoSocialServer;
+    var token = req.body.tokenServer;
+    var cnpj = req.body.cnpjServer;
+    var email_empresa = req.body.email_empresaServer;
+    var telefone = req.body.telefoneServer;
+    var estado = req.body.estadoServer;
+    var cep = req.body.cepServer;
+    var numero = req.body.numeroServer;
+    var complemento = req.body.complementoServer;
+
+
+    // Faça as validações dos valores
+    if (razaoSocial == undefined) {
+        res.status(400).send("Razao social está undefined!");
+    } else if (token == undefined) {
+        res.status(400).send("token está undefined!");
+    } else if (cnpj == undefined) {
+        res.status(400).send("cnpj está undefined!");
+    } else if (email_empresa == undefined) {
+        res.status(400).send("email está undefined!");
+    } else if (telefone == undefined) {
+        res.status(400).send("telefone está undefined!");
+    } else if (estado == undefined) {
+        res.status(400).send("estado está undefined!");
+    } else if (cep == undefined) {
+        res.status(400).send("cep está undefined!");
+    } else if (numero == undefined) {
+        res.status(400).send("numero está undefined!");
+    }else {
+
+        // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
+        empresaModel.cadastrarEmpresa(razaoSocial, token, cnpj, email_empresa, telefone)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+
+                    var fk_end_empresa = resultado.insertId;
+
+                    empresaModel.cadastrarEndereco(estado, cep, numero, complemento, fk_end_empresa)
+                    .then(
+                        function () {
+                            res.status(201).json({
+                                message: 'Cadastro de serviços ok!',
+                                empresaId: fk_end_empresa
+                            })
+                        }
+                    )
+                    .catch(function (erroServicos) {
+                        res.status(500).json(erroServicos.sqlMessage);
+                    })
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log(
+                        "\nHouve um erro ao realizar o cadastro! Erro: ",
+                        erro.sqlMessage
+                    );
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+    }
+}
+
+function buscarPorCnpj(req, res) {
+  var cnpj = req.query.cnpj;
+
+  empresaModel.buscarPorCnpj(cnpj).then((resultado) => {
+    res.status(200).json(resultado);
+  });
+}
+
+function listar(req, res) {
+  empresaModel.listar().then((resultado) => {
+    res.status(200).json(resultado);
+  });
+}
+
+function buscarPorId(req, res) {
+  var id = req.params.id;
+
+  empresaModel.buscarPorId(id).then((resultado) => {
+    res.status(200).json(resultado);
+  });
+}
+
+
+
+module.exports = {
+  cadastrarEmpresa,
+  buscarPorCnpj,
+  buscarPorId,
+  listar,
+};
---
+++ src/models/empresaModel.js
@@ -0,0 +1,47 @@
+var database = require("../database/config");
+
+function cadastrarEmpresa( razaoSocial, token, cnpj, email_empresa, telefone) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", razaoSocial, token, cnpj, email_empresa, telefone);
+    
+    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+    //  e na ordem de inserção dos dados.
+    var instrucaoSql = `
+        INSERT INTO empresa (razaoSocial, token, cnpj, email, telefone) VALUES 
+        ('${razaoSocial}', '${token}', '${cnpj}', '${email_empresa}', '${telefone}');
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+function cadastrarEndereco(estado, cep, numero, complemento, fk_end_empresa) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", estado, cep, numero, complemento, fk_end_empresa);
+    
+    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+    //  e na ordem de inserção dos dados.
+    var instrucaoSql = `
+        INSERT INTO endereco (estado, cep, numeroResidencial, complemento, fk_end_empresa) VALUES 
+        ('${estado}', '${cep}', '${numero}','${complemento}','${fk_end_empresa}');
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+function buscarPorId(id) {
+  var instrucaoSql = `SELECT * FROM empresa WHERE idEmpresa = '${id}'`;
+
+  return database.executar(instrucaoSql);
+}
+
+function listar() {
+  var instrucaoSql = `SELECT idEmpresa, razaoSocial, cnpj, token FROM empresa`;
+
+  return database.executar(instrucaoSql);
+}
+
+function buscarPorCnpj(cnpj) {
+  var instrucaoSql = `SELECT * FROM empresa WHERE cnpj = '${cnpj}'`;
+
+  return database.executar(instrucaoSql);
+}
+
+module.exports = { cadastrarEndereco, cadastrarEmpresa, buscarPorCnpj, buscarPorId, listar };
